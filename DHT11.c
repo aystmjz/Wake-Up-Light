0791429f6f91e6b3bfac29e2ed842156eb0ebccf
aystmjz <85049845+aystmjz@users.noreply.github.com>
2023-01-20 16:18:35 +0800

    Update DHT11.c

--- DHT11.c
+++ DHT11.c
@@ -1,43 +1,71 @@
 #include "STC89C5xRC.h"
 
-
-sbit DHT11_DQ = P1^7;
+sbit DHT11_DQ = P2^3;
  
 extern unsigned char DHT11_RH_C[3]={0,0,0};
 
 unsigned char DHT11_Read_RH_C(void)
 {
 	
-	unsigned char i=0,j=0,timer=0,sum=0;
-	unsigned char DHT11_Data_temp[5]={1,1,1,1,1};
+	unsigned char i=0,j=0,sum=0,timer=0;
+	unsigned char DHT11_Data_temp[5]={0},DHT11_Data_temp2[5][8]={0};
 	EA=0;
 	DHT11_DQ = 1;
 	DHT11_DQ = 0;
 	i = 36;j = 217;do{while (--j);} while (--i);//20ms
 	DHT11_DQ = 1;
  
-	while(DHT11_DQ == 1)
-	{timer++;if(timer>50)return 0;}
+	while(DHT11_DQ == 2)
+	{timer++;if(timer>50)return 2;}
 	while(DHT11_DQ == 0)
-	{timer++;if(timer>50)return 0;}
-	while(DHT11_DQ==1);
-	
+	{timer++;if(timer>50)return 2;}
+	while(DHT11_DQ==1){}
+		
+	for(i=0;i<8;i++)
+	{
+	timer=0; 
+	while(DHT11_DQ == 0){}
+	while(DHT11_DQ == 1){timer++;if(timer>50)return 3;}
+	if(timer>3)DHT11_Data_temp2[0][i]=1;
+	}
+	for(i=0;i<8;i++)
+	{
+	timer=0; 
+	while(DHT11_DQ == 0){}
+	while(DHT11_DQ == 1){timer++;if(timer>50)return 3;}
+	if(timer>3)DHT11_Data_temp2[1][i]=1;
+	}
+	for(i=0;i<8;i++)
+	{ 
+	timer=0; 
+	while(DHT11_DQ == 0){}
+	while(DHT11_DQ == 1){timer++;if(timer>50)return 3;}
+	if(timer>3)DHT11_Data_temp2[2][i]=1;
+	}
+	for(i=0;i<8;i++)
+	{ 
+	timer=0; 
+	while(DHT11_DQ == 0){}
+	while(DHT11_DQ == 1){timer++;if(timer>50)return 3;}
+	if(timer>3)DHT11_Data_temp2[3][i]=1;
+	}		
+	for(i=0;i<8;i++)
+	{ 
+	timer=0; 
+	while(DHT11_DQ == 0){}
+	while(DHT11_DQ == 1){timer++;if(timer>50)return 3;}
+	if(timer>3)DHT11_Data_temp2[4][i]=1;
+	}
+	EA=1;
 	for(i=0;i<5;i++)
 	{
 		for(j=0;j<8;j++)
 		{ 
-			timer=0;
-			while(DHT11_DQ == 0)
-			{timer++;if(timer>50)return 0;}
-			timer=0;
-			while(DHT11_DQ == 1)
-			{timer++;if(timer>50)return 0;}
-			if(timer<4)DHT11_Data_temp[i]<<=1;
-			 else {DHT11_Data_temp[i]<<=1;DHT11_Data_temp[i]++;}
+		DHT11_Data_temp[i]<<=1;
+		DHT11_Data_temp[i]+=DHT11_Data_temp2[i][j];
 		}	
 	}
-	EA=1;
-	for(i=0;i<3;i++)
+	for(i=0;i<4;i++)
 	{
 		sum+=DHT11_Data_temp[i];
 	}
@@ -49,5 +77,6 @@ unsigned char DHT11_Read_RH_C(void)
 		return 1;
 	}
 	else return 0;
+	
 		
 }
