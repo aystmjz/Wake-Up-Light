202be1620aaa0d19ec27e80ea60521ee0350ce70
aystmjz <85049845+aystmjz@users.noreply.github.com>
2023-03-04 20:02:10 +0800

    Update Timer0.c

--- Timer0.c
+++ Timer0.c
@@ -1,34 +1,34 @@
 #include "STC89C5xRC.h"
 
 
-void Timer0_Init(void)		//1����@11.0592MHz
+void Timer0_Init(void)		//1毫秒@11.0592MHz
 {
 
-	TMOD &= 0xF0;		//���ö�ʱ��ģʽ
-	TMOD |= 0x01;		//���ö�ʱ��ģʽ
-	TL0 = 0x66;		//���ö�ʱ��ʼֵ
-	TH0 = 0xFC;		//���ö�ʱ��ʼֵ
-	TF0 = 0;		//���TF0��־
-	TR0 = 1;		//��ʱ��0��ʼ��ʱ
+	TMOD &= 0xF0;		//设置定时器模式
+	TMOD |= 0x01;		//设置定时器模式
+	TL0 = 0x66;		//设置定时初始值
+	TH0 = 0xFC;		//设置定时初始值
+	TF0 = 0;		//清除TF0标志
+	TR0 = 1;		//定时器0开始计时
 	ET0=1;
 	EA=1;
 	
 	PT0=0;
 }
 
-void Timer2_Init(void)		//50΢��@11.0592MHz
+void Timer2_Init(void)		//50微秒@11.0592MHz
 {
-	T2MOD = 0;		//��ʼ��ģʽ�Ĵ���
-	T2CON = 0;		//��ʼ�����ƼĴ���
-	TL2 = 0xD2;		//���ö�ʱ��ʼֵ
-	TH2 = 0xFF;		//���ö�ʱ��ʼֵ
-	RCAP2L = 0xD2;		//���ö�ʱ����ֵ
-	RCAP2H = 0xFF;		//���ö�ʱ����ֵ
-	TR2 = 1;		//��ʱ��2��ʼ��ʱ
+	T2MOD = 0;		//初始化模式寄存器
+	T2CON = 0;		//初始化控制寄存器
+	TL2 = 0xD2;		//设置定时初始值
+	TH2 = 0xFF;		//设置定时初始值
+	RCAP2L = 0xD2;		//设置定时重载值
+	RCAP2H = 0xFF;		//设置定时重载值
+	TR2 = 1;		//定时器2开始计时
 		ET2=0;
 	EA=1;
 	
-	//PT2H=1;//���ȼ�3
+	//PT2H=1;//优先级3
 	PT2=1;
 }
 
@@ -39,19 +39,6 @@ void Timer_Init(void)
 	Timer2_Init();	
 }
 
-
-//void Timer2Init(void)		//100΢��@12.000MHz
-//{
-//	T2MOD = 0;		//��ʼ��ģʽ�Ĵ���
-//	T2CON = 0;		//��ʼ�����ƼĴ���
-//	TL2 = 0x9C;		//���ö�ʱ��ʼֵ
-//	TH2 = 0xFF;		//���ö�ʱ��ʼֵ
-//	RCAP2L = 0x9C;		//���ö�ʱ����ֵ
-//	RCAP2H = 0xFF;		//���ö�ʱ����ֵ
-//	TR2 = 1;		//��ʱ��2��ʼ��ʱ
-//	ET2=1;
-//	EA=1;
-//}
 
 
 /*
