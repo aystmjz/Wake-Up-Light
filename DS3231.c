8305582422ec4896aa2b33face0a06d13af5031c
aystmjz <85049845+aystmjz@users.noreply.github.com>
2023-03-04 19:51:44 +0800

    Update DS3231.c

--- DS3231.c
+++ DS3231.c
@@ -4,15 +4,19 @@
 #define DS3231_ADDRESS 0xD0
 #define DS3231_CONTROL 0x0E
 #define DS3231_STATUS  0x0F
-#define DS3231_Temp    0x0B
+#define DS3231_Temp	   0x0B
+#define DS3231_Temp2   0x0C
 
 unsigned char code RTC_CLOCK_ADDR[7]={0x00,0x01,0x02,0x04,0x05,0x03,0x06};//秒分时日月周年
 unsigned char code RTC_ALARM1_ADDR[4]={0x07,0x08,0x09,0x0A};//秒分时日月周年
 unsigned char code RTC_ALARM2_ADDR[3]={0x0B,0x0C,0x0D};//秒分时日月周年
-extern unsigned char TIME[7] = {0, 0, 0x16, 0x1C, 0x06, 0x01, 0x17};//秒分时日月周年
+extern char TIME[7] = {0, 0, 0x16, 0x1C, 0x06, 0x01, 0x17};//秒分时日月周年
 extern unsigned char Alarm_Set[8]={0,1,1,1,1,1,0,1};
-extern unsigned char Alarm_Date[2]={8,0};
+extern char Alarm_Date[2]={8,0};
+extern char Alarm_Date_Temp[2]={8,0};
 extern unsigned char Alarm_Status[2]={0,0};
+extern unsigned char Alarm_Enable=1;
+extern char PWM_Mod=0;
 
 void DS3231_WriteByte(unsigned char WordAddress,unsigned char Data)
 {
@@ -66,35 +70,59 @@ void DS3231_WriteTime()
 
 void DS3231_WriteAlarm()
 {
-   unsigned char i,Temp=0;
-	 DS3231_WriteByte(RTC_ALARM1_ADDR[0],0);
-	 DS3231_WriteByte(RTC_ALARM1_ADDR[3],0x80);
-	 for(i=0;i<8;i++)
-	 {
-			if(i==1&&i==2)DS3231_WriteByte(RTC_ALARM1_ADDR[i],((Alarm_Date[2-i]/10)<<4)+(Alarm_Date[2-i]%10));
-			Temp+=Alarm_Set[7-i];
-			if(i!=7)Temp<<=1;
-	 }
-		DS3231_WriteByte(DS3231_Temp,Temp);
+	unsigned char i,Temp=0;
+	DS3231_WriteByte(RTC_ALARM1_ADDR[0],0);
+	DS3231_WriteByte(RTC_ALARM1_ADDR[3],0x80);
+	for(i=0;i<8;i++)
+	{
+		if(i==1||i==2)DS3231_WriteByte(RTC_ALARM1_ADDR[i],((Alarm_Date_Temp[2-i]/10)<<4)+(Alarm_Date_Temp[2-i]%10));
+		Temp+=Alarm_Set[7-i];
+		if(i!=7)Temp<<=1;
+	}
+	DS3231_WriteByte(DS3231_CONTROL,0x04+Alarm_Enable);
+	DS3231_WriteByte(DS3231_Temp2,PWM_Mod);
+	DS3231_WriteByte(DS3231_Temp,Temp);
 }
 
-void DS3231_Init()//DS3231初始化函数
+void DS3231_Init()
 {
 	unsigned char i,Temp;
-	DS3231_WriteByte(DS3231_CONTROL,0x05);
 	DS3231_WriteByte(DS3231_STATUS,0x00);
 	DS3231_ReadTime();
 	
+	PWM_Mod=DS3231_ReadByte(DS3231_Temp2);
+	
+	Temp=DS3231_ReadByte(DS3231_CONTROL);
+	Alarm_Enable=!(!(Temp&0x01));
+
 	Temp=DS3231_ReadByte(DS3231_Temp);
 	for(i=0;i<8;i++)
 	{	
-		Alarm_Set[i]=(Temp|(0x01<<i));
+		Alarm_Set[i]=!(!(Temp&(0x01<<i)));
 	}
 
-	for(i=1;i<2;i++)
+	for(i=1;i<=2;i++)
 	{
 		Temp=DS3231_ReadByte(RTC_ALARM1_ADDR[i]);
-		Alarm_Date[2-i]=(Temp>>4)*10+(Temp&0x0F);
+		Alarm_Date_Temp[2-i]=(Temp>>4)*10+(Temp&0x0F);
+	}
+	
+	switch(PWM_Mod)
+	{	
+		case 0: Alarm_Date[0]=(Alarm_Date_Temp[0]*60+Alarm_Date_Temp[1]+5)/60;
+				Alarm_Date[1]=(Alarm_Date_Temp[0]*60+Alarm_Date_Temp[1]+5)%60;break;
+		case 1: Alarm_Date[0]=(Alarm_Date_Temp[0]*60+Alarm_Date_Temp[1]+10)/60;
+				Alarm_Date[1]=(Alarm_Date_Temp[0]*60+Alarm_Date_Temp[1]+10)%60;break;
+		case 2: Alarm_Date[0]=(Alarm_Date_Temp[0]*60+Alarm_Date_Temp[1]+15)/60;
+				Alarm_Date[1]=(Alarm_Date_Temp[0]*60+Alarm_Date_Temp[1]+15)%60;break;
+		case 3: Alarm_Date[0]=(Alarm_Date_Temp[0]*60+Alarm_Date_Temp[1]+20)/60;
+				Alarm_Date[1]=(Alarm_Date_Temp[0]*60+Alarm_Date_Temp[1]+20)%60;break;
+		case 4: Alarm_Date[0]=(Alarm_Date_Temp[0]*60+Alarm_Date_Temp[1]+30)/60;
+				Alarm_Date[1]=(Alarm_Date_Temp[0]*60+Alarm_Date_Temp[1]+30)%60;break;
+		case 5: Alarm_Date[0]=(Alarm_Date_Temp[0]*60+Alarm_Date_Temp[1]+40)/60;
+				Alarm_Date[1]=(Alarm_Date_Temp[0]*60+Alarm_Date_Temp[1]+40)%60;break;
+		case 6: Alarm_Date[0]=(Alarm_Date_Temp[0]*60+Alarm_Date_Temp[1]+60)/60;
+				Alarm_Date[1]=(Alarm_Date_Temp[0]*60+Alarm_Date_Temp[1]+60)%60;break;
 	}
 	
 	
